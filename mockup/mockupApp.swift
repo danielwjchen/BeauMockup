1d8d475af86bdfb895f8f30b8906c971917e102e
Daniel Chen <hello@danielwjchen.com>
2026-03-03 21:53:31 -0600

    Initial Commit

---
+++ mockup/mockupApp.swift
@@ -0,0 +1,17 @@
+//
+//  mockupApp.swift
+//  mockup
+//
+//  Created by Daniel Chen on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct mockupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
